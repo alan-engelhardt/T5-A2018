8c74c125580df9fbcb00b784a88a2dfb235ec2c7
Alan Engelhardt <Alan@alansmbamid2014.home>
2018-09-07 20:53:09 +0200

    Add in-stock filter

--- scripts.js
+++ scripts.js
@@ -7,108 +7,112 @@ const pLink = "https://kea-alt-del.dk/t5/api/product?id=";
 
 const modal = document.querySelector("#modal");
 const discountFilter = document.querySelector("#discount-filter");
+const instockFilter = document.querySelector("#instock-filter");
 
 const main = document.querySelector("main");
 const nav = document.querySelector("nav");
 const allLink = document.querySelector("#allLink");
 const myTemplate = document.querySelector("#myTemplate").content;
 
-let filters = [];
+let curentFilter;
 
-allLink.addEventListener("click", ()=>filterBy("all"));
+allLink.addEventListener("click", () => filterBy("all"));
+discountFilter.addEventListener("click", () => filter("has-discount"));
+instockFilter.addEventListener("click", () => filter("in-stock"));
 
-function filterBy(category){
-	filters.push("catgory");
-	document.querySelectorAll("section").forEach(section=>{
-		if(section.id == category || category == "all"){
-			section.classList.remove("hide");
-			section.querySelectorAll("article").forEach(article=>article.classList.remove("hide"));
-		}else{
-			section.classList.add("hide");
-		}
-	})
-}
-
-discountFilter.addEventListener("click", ()=>{
-	filters.push("discount");
+function filterBy(category) {
+	curentFilter = "category";
 	document.querySelectorAll("section").forEach(section => {
-		if(section.classList.contains("has-discount")){
+		if (section.id == category || category == "all") {
 			section.classList.remove("hide");
-			section.querySelectorAll("article").forEach(article=>{
-				if(!article.classList.contains("has-discount")){
-					article.classList.add("hide");
-				}
-			});
-		}else{
+			section.querySelectorAll("article").forEach(article => article.classList.remove("hide"));
+		} else {
 			section.classList.add("hide");
 		}
 	});
-});
+	console.log(curentFilter);
+}
 
-fetch(catLink).then(promise=>promise.json()).then(data=>buildCategories(data));
 
-function buildCategories(data){
-	data.forEach(category=>{
+function filter(criteria) {
+	curentFilter = criteria;
+	document.querySelectorAll("section").forEach(section => {
+		if (section.classList.contains(criteria)) {
+			section.classList.remove("hide");
+			section.querySelectorAll("article").forEach(article => {
+				if (!article.classList.contains(criteria)) {
+					article.classList.add("hide");
+				}
+			});
+		} else {
+			section.classList.add("hide");
+		}
+	});
+	console.log(curentFilter);
+}
+
+fetch(catLink).then(promise => promise.json()).then(data => buildCategories(data));
+
+function buildCategories(data) {
+	data.forEach(category => {
 		//console.log(category);
 		const newSection = document.createElement("section");
 		const newH2 = document.createElement("h2");
 		const newLink = document.createElement("a");
-		newLink.href="#";
-		newLink.textContent=category;
-		newLink.addEventListener("click", ()=>filterBy(category));
-		newSection.id=category;
-		newH2.textContent=category;
+		newLink.href = "#";
+		newLink.textContent = category;
+		newLink.addEventListener("click", () => filterBy(category));
+		newSection.id = category;
+		newH2.textContent = category;
 		nav.appendChild(newLink);
 		newSection.appendChild(newH2);
 		main.appendChild(newSection);
 	});
-	fetch(pListLink).then(promise=>promise.json()).then(data=>show(data));
+	fetch(pListLink).then(promise => promise.json()).then(data => show(data));
 }
 
-function show(plist){
-	plist.forEach(product=>{
-		const parent = document.querySelector("#"+product.category);
+function show(plist) {
+	plist.forEach(product => {
+		const parent = document.querySelector("#" + product.category);
 		const clone = myTemplate.cloneNode(true);
-		clone.querySelector("h2").textContent=product.name;
-		clone.querySelector(".description").textContent=product.shortdescription;
-		clone.querySelector(".price span").textContent=product.price;
-		clone.querySelector(".productImage").src=imgbase + "small/" + product.image + "-sm.jpg";
-		clone.querySelector(".details-button").addEventListener("click", ()=>{
+		clone.querySelector("h2").textContent = product.name;
+		clone.querySelector(".description").textContent = product.shortdescription;
+		clone.querySelector(".price span").textContent = product.price;
+		clone.querySelector(".productImage").src = imgbase + "small/" + product.image + "-sm.jpg";
+		clone.querySelector(".details-button").addEventListener("click", () => {
 			modal.classList.remove("hide");
-			fetch(pLink+product.id).then(promise=>promise.json()).then(data=>showDetails(data))
+			fetch(pLink + product.id).then(promise => promise.json()).then(data => showDetails(data))
 		});
 
-		if(product.discount){
+		if (product.discount) {
 			const newPrice = Math.round(product.price - product.price * product.discount / 100);
 			clone.querySelector(".discount-price span").textContent = newPrice;
+			clone.querySelector(".discount-price").classList.remove("hide");
 			clone.querySelector("article").classList.add("has-discount");
 			parent.classList.add("has-discount");
-		} else {
-			clone.querySelector(".discount-price").classList.add("hide");
 		}
 
-		if(product.soldout){
+		if (product.soldout) {
 			clone.querySelector(".soldout").classList.remove("hide");
 			clone.querySelector("article").classList.add("is-soldout");
+			parent.classList.add("is-soldout");
+		} else {
+			clone.querySelector("article").classList.add("in-stock");
+			parent.classList.add("in-stock");
 		}
 
 		parent.appendChild(clone);
 	});
 }
 
-function showDetails(product){
-	modal.querySelector("h2").textContent=product.name;
-	modal.querySelector("p").textContent=product.longdescription;
+function showDetails(product) {
+	modal.querySelector("h2").textContent = product.name;
+	modal.querySelector("p").textContent = product.longdescription;
 }
 
-modal.addEventListener("click", ()=>modal.classList.add("hide"));
-
-
-
-
-
-
-
-
-
-
+modal.addEventListener("click", e => {
+	console.log(e.target);
+	modal.classList.add("hide");
+	modal.querySelector("h2").textContent = "";
+	modal.querySelector("p").textContent = "";
+});
