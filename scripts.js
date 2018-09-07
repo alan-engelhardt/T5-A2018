479413f45d7c76965f746ee4c616e23fefbc79bf
Alan Engelhardt <Alan@alansmbamid2014.home>
2018-09-07 16:20:51 +0200

    combine category and discount filters

--- scripts.js
+++ scripts.js
@@ -14,13 +14,31 @@ const myTemplate = document.querySelector("#myTemplate").content;
 
 allLink.addEventListener("click", ()=>filterBy("all"));
 
-discountFilter.addEventListener("click", () => {
-	document.querySelectorAll("article").forEach(article => {
-		if (!article.classList.contains('has-discount')) {
-			article.classList.toggle("hide");
+function filterBy(category){
+	document.querySelectorAll("section").forEach(section=>{
+		if(section.id == category || category == "all"){
+			section.classList.remove("hide");
+			section.querySelectorAll("article").forEach(article=>article.classList.remove("hide"));
+		}else{
+			section.classList.add("hide");
 		}
 	})
-})
+}
+
+discountFilter.addEventListener("click", ()=>{
+	document.querySelectorAll("section").forEach(section => {
+		if(section.classList.contains("has-discount")){
+			section.classList.remove("hide");
+			section.querySelectorAll("article").forEach(article=>{
+				if(!article.classList.contains("has-discount")){
+					article.classList.add("hide");
+				}
+			})
+		}else{
+			section.classList.add("hide");
+		}
+	});
+});
 
 fetch(catLink).then(promise=>promise.json()).then(data=>buildCategories(data));
 
@@ -42,16 +60,6 @@ function buildCategories(data){
 	fetch(pListLink).then(promise=>promise.json()).then(data=>show(data));
 }
 
-function filterBy(category){
-	document.querySelectorAll("section").forEach(section=>{
-		if(section.id == category || category == "all"){
-			section.classList.remove("hide");
-		}else{
-			section.classList.add("hide");
-		}
-	})
-}
-
 function show(plist){
 	plist.forEach(product=>{
 		const parent = document.querySelector("#"+product.category);
@@ -66,11 +74,11 @@ function show(plist){
 			const newPrice = Math.round(product.price - product.price * product.discount / 100);
 			clone.querySelector(".discount-price span").textContent = newPrice;
 			clone.querySelector("article").classList.add("has-discount");
+			parent.classList.add("has-discount");
 		} else {
 			clone.querySelector(".discount-price").classList.add("hide");
 			//console.log("Not on discount")
 		}
-
 		parent.appendChild(clone);
 	});
 }
